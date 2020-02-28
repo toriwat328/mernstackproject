f321ef3b35b4e57f078a125c68ded36a8b8a4b93
toriwat328 <toriwat328@gmail.com>
2020-02-28 09:11:44 -0500

    continuing to set up redux added reducer and action folders and files set up store.js file

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 import AppNavbar from './components/appNavBar.js';
 import ShoppingList from './components/shoppingList.js';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 function App() {
   return (
+     <Provider store={store}>
     <div className="App">
      <AppNavbar/>
      <ShoppingList/>
     </div>
+    </Provider>
   );
 }
 
--- client/src/store.js
+++ client/src/store.js
@@ -7,7 +7,10 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initalState, compose(
-    applyMiddleware(..middleware),
-    
+const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 ));
+
+export default store;
