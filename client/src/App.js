d8fe49ed8aac0013b5ba8e89bae224d2f12bd338
toriwat328 <toriwat328@gmail.com>
2020-02-27 19:43:17 -0500

    added shopping list component to show items and implemented add and remove item functionality - updated app.css to give items animations as users add and delete items

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppNavbar from './components/appNavBar.js';
+import ShoppingList from './components/shoppingList.js';
 
 
 function App() {
   return (
     <div className="App">
      <AppNavbar/>
+     <ShoppingList/>
     </div>
   );
 }
